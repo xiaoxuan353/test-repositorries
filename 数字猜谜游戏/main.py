48f0f6e47a0955b5175b3078824ce4c1fd490409
1096193045@qq.com <1096193045@qq.com>
2025-02-16 15:48:15 +0800

    优化后的结果

--- 数字猜谜游戏/main.py
+++ 数字猜谜游戏/main.py
@@ -1,20 +1,18 @@
 import random
 goal_number=random.randint(0,100)#包含0到100
-for i in range(7):#只能猜7次
-    try:
+try:
+    for i in range(7):#只能猜7次
         number = int(input('请输入[0,100]间的整数'))
         if number > goal_number:
             print(f'大了,还能猜{6-i}次')
-        if i == 6:
-            print('game over!')
         elif number < goal_number:
-            print(f'小了,还能猜{6-i}次')
-        if i == 6:
-            print('game over!')
+            print(f'小了,还能猜{6-i}次')   
         else:
-           print(f'答对了,正确答案是{goal_number}')
-        break
-    except:
-        print('请输入正确整数')
+            print(f'答对了,正确答案是{goal_number}')
+            break
+    if i == 6:
+            print('game over!')
+except:
+    print('请输入正确整数')
     
     
